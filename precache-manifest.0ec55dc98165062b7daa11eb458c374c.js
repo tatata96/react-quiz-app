a849dd49669c6b4fb9bb0d548bfafba3205202c8
tamara <tamarakozok@gmail.com>
2021-08-05 14:05:09 +0300

    Updates

---
+++ precache-manifest.0ec55dc98165062b7daa11eb458c374c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8bc88da76ddbc5a1e447696178cc5da",
+    "url": "/react-quiz-app/index.html"
+  },
+  {
+    "revision": "d1acd8e19578e2120169",
+    "url": "/react-quiz-app/static/css/main.25bfa961.chunk.css"
+  },
+  {
+    "revision": "15a42ebbc7cbc06b1c96",
+    "url": "/react-quiz-app/static/js/2.89f7c905.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/react-quiz-app/static/js/2.89f7c905.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1acd8e19578e2120169",
+    "url": "/react-quiz-app/static/js/main.aa7176ed.chunk.js"
+  },
+  {
+    "revision": "a1a5e3f5c3f2af766c7d",
+    "url": "/react-quiz-app/static/js/runtime-main.0bd8c3fa.js"
+  }
+]);
